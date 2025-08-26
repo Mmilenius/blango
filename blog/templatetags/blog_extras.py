09194ba1e559c7dee21b7e3ac932316c6a442313
Лукомський Русланович <maksym.lukomskyi.pp.2022@lpnu.ua>
2025-08-26 20:56:58 +0000

    Finish crispy forms

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django import template
+register = template.Library()
+user_model = get_user_model()
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    """Створює відкриваючий тег рядка"""
+    return f'<div class="row {extra_classes}">'
+
+@register.simple_tag
+def col():
+    """Створює відкриваючий тег колонки"""
+    return '<div class="col">'
+
+@register.simple_tag
+def endrow():
+    """Закриваючий тег для рядка"""
+    return '</div>'
+
+@register.simple_tag
+def endcol():
+    """Закриваючий тег для колонки"""
+    return '</div>'
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    """Додає CSS клас до поля форми"""
+    return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    return name
