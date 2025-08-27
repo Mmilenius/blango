1e98f02427af672f95144150e8afd26ab834a630
Лукомський Русланович <maksym.lukomskyi.pp.2022@lpnu.ua>
2025-08-27 11:39:19 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 import blog.views
+
+print(f"Time zone: {settings.TIME_ZONE}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
